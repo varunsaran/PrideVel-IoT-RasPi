c4f22ef6b42ccd4b0ae1e7968c9e04f87af20373
Varun Saran <Varun Saran>
2017-07-27 14:43:24 -0700

    commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static(__dirname + '/routes'));
+app.use(express.static(__dirname + '/routes'));
 //previous line added to make home page
 app.use('/', index);
 app.use('/users', users);
