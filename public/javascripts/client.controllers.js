c0f26d79124c8962967fcb1cb175c20442a25d5e
Varun Saran <Varun Saran>
2017-07-28 15:08:43 -0700

    m

--- public/javascripts/client.controllers.js
+++ public/javascripts/client.controllers.js
@@ -116,7 +116,7 @@ window.onload = function () {
           var date = payload.timer;
           console.log(date);
 					dps.push({
-						x: value.toString(date),
+						x: String(date),
 						y: parseFloat(payload.value)
 				});
 				}
